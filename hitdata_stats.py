4ea3968d8dd695283e1741a901f4e15f4acd1a7e
josh daly <joshua.daly@uqconnect.edu.au>
2015-04-06 14:16:42 +1000

    phylum interactions matrix

--- hitdata_stats.py
+++ hitdata_stats.py
@@ -105,7 +105,7 @@ class HitDataStats(object):
                 matrix[i][v] = hits
         
         # make scatter plot
-        fig = plt.figure(figsize=(30,15),dpi=300)
+        fig = plt.figure(figsize=(30,15),dpi=1200)
         ax = plt.subplot(1,1,1,axisbg='white',autoscale_on=True, aspect='equal')#, xlim=[-0.2,1.2], ylim=[-0.2,1.2])
         xs      = []
         ys      = []
